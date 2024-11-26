4d2d47fed49a6563926eabdb36bc2dd7bbb291f9
zzinue <zzinue@gmail.com>
2024-11-25 21:01:26 -0600

    agregando al proyecto

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
     <div className="App">
       <h1>hola mundo </h1>
       <li>estas es una lista</li>
-      <p>hola</p>
+      <h2>Este es un subtitulo</h2>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
     </div>
   );
 }
